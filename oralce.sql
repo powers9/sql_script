6a8b946280778b66cc62eb3d5914d6d5ad83e7ed
powers9 <samtrmn@gmail.com>
2022-10-26 09:15:06 +0530

    oracle sql commands

---
+++ oralce.sql
@@ -0,0 +1,9 @@
+
+------ Get execution plan detail for table--------
+explain plan for
+select * from table where condition
+select from plan table
+-----------------------------------------------
+---------------json query---------------------
+JSON VALUE(RESPONSE, '$.header.msgBdy.data [8]') as res,
+--------------json query---------------------
